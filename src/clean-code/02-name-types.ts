e670b7401ce0fbe637238cc4fff54b5a165bb71c
Luis Angel <angelfgdeveloper@gmail.com>
2023-04-23 19:20:51 -0600

    clean code - names-types resuelto

--- src/clean-code/02-name-types.ts
+++ src/clean-code/02-name-types.ts
@@ -1,53 +1,53 @@
 (() => {
   // arreglo de temperaturas celsius
-  const arrayOfNums = [33.6, 12.34];
+  const amountsOfCelsiusTemperatures = [33.6, 12.34];
 
   // Dirección ip del servidor
-  const ip = '123.123.123.123';
+  const ipAddressOfServer = '123.123.123.123';
 
   // Listado de usuarios
-  const people = [
+  const users = [
     { id: 1, email: 'fernando@google.com' },
     { id: 2, email: 'juan@google.com' },
     { id: 3, email: 'melissa@google.com' },
   ];
 
   // Listado de emails de los usuarios
-  const emails = people.map((u) => u.email);
+  const userEmails = users.map((user) => user.email);
 
   // Variables booleanas de un video juego
-  const jump = false;
-  const run = true;
-  const noTieneItems = true;
-  const loading = false;
+  const canJump = false;
+  const canRun = true;
+  const hasItems = false;
+  const isLoading = true;
 
   // Otros ejercicios
   // tiempo inicial
-  const start = new Date().getTime();
+  const startTime = new Date().getTime();
   //....
   // 3 doritos después
   //...
   // Tiempo al final
-  const end = new Date().getTime() - start;
+  const endTime = new Date().getTime() - startTime;
 
   // Funciones
   // Obtiene los libros
-  function book() {
+  function getBooks() {
     throw new Error('Function not implemented.');
   }
 
   // obtiene libros desde un URL
-  function BooksUrl(u: string) {
+  function getBooksByUrl( url: string ) {
     throw new Error('Function not implemented.');
   }
 
   // obtiene el área de un cuadrado basado en sus lados
-  function areaCuadrado(s: number) {
+  function getSquareArea( side: number ) {
     throw new Error('Function not implemented.');
   }
 
   // imprime el trabajo
-  function printJobIfJobIsActive() {
+  function printJob() {
     throw new Error('Function not implemented.');
   }
   
