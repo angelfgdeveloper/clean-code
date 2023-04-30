3540988a1c3b3850e2df2d7341e4cb1ed77be4a7
Luis Angel <angelfgdeveloper@gmail.com>
2023-04-30 13:52:04 -0600

    clean code - clases-a no usando responsabilidad unica con poo

--- src/clean-code/06-clases-a.ts
+++ src/clean-code/06-clases-a.ts
@@ -1,6 +1,8 @@
 
 (() => {
 
+  // No aplicando el principio de responsabilidad unica
+
   type Gender = 'M' | 'F';
 
   // 1 - Forma larga
@@ -21,14 +23,67 @@
   class Person {
 
     constructor( 
-      public name: string,
-      public gender: Gender,
+      public name     : string,
+      public gender   : Gender,
       public birthdate: Date,
     ) {}
 
   }
 
-  const newPerson = new Person('Angel', 'M', new Date('1994-12-10'));
-  console.log(newPerson);
+  // const newPerson = new Person('Angel', 'M', new Date('1994-12-10'));
+  // console.log(newPerson);
+
+  class User extends Person {
+    public lastAccess: Date;
+
+    constructor(
+      public email      : string,
+      public role       : string,
+
+      //** Heredados
+      name              : string,
+      gender            : Gender,
+      birthdate         : Date,
+    ) {
+      super(name, gender, birthdate);
+      this.lastAccess = new Date();
+    }
+
+    checkCredentials() {
+      return true;
+    }
+
+  }
+
+  class UserSettings extends User {
+
+    constructor(
+      public workingDirectory: string,
+      public lastOpenFolder  : string,
+
+      //** Heredados
+      email                  : string,
+      role                   : string,
+      name                   : string,
+      gender                 : Gender,
+      birthdate              : Date,
+    ) {
+      super(email, role, name, gender, birthdate);
+    }
+
+  }
+
+  const userSettings = new UserSettings(
+    '/usr/home',
+    '/home',
+    'angel@google.com',
+    'Admin',
+    'Angel',
+    'M',
+    new Date('1996-10-12')
+  );
+
+  // console.log({ userSettings, areCredentialsValid: userSettings.checkCredentials() });
+  console.log({ userSettings });
 
 })();
