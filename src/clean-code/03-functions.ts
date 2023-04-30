2d8d2d9f1f8956f004fada70b68346ed82ec0507
Luis Angel <angelfgdeveloper@gmail.com>
2023-04-30 11:40:26 -0600

    clean code - functions resuelto con lineas de funcion

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -36,6 +36,38 @@
       return true;
   }
 
+  // Continuacion...
+
+  // const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+
+  //   let result: number; // No dejarlo any
+
+  //   if ( isDead ) {
+  //       result = 1500;
+  //   } else {
+  //       if ( isSeparated ) {
+  //           result = 2500;
+  //       } else {
+  //           if ( isRetired ) {
+  //               result = 3000;
+  //           } else {
+  //               result = 4000; 
+  //           }
+  //       }
+  //   }
+    
+  //   return result;
+  // }
+
+  const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+    
+    if ( isDead ) return 1500;
+    
+    if ( isSeparated ) return 2500;
+
+    return ( isRetired ) ? 3000 : 4000;
+  }
+
 })();
 
 
