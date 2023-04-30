c625dc76c89c81880fb96a5689b418cd25653c90
Luis Angel <angelfgdeveloper@gmail.com>
2023-04-30 10:31:32 -0600

    clean code - functions resuelto

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,36 +1,39 @@
 (() => {
 
-  // función para obtener información de una película por Id
-  function getAllMovies( movieId: string ) {
-      console.log({ movieId });
-  }
-
-  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-  function getAllMovieActors( id: string ) {
+  function getMovieById( id: string ) {
       console.log({ id });
   }
 
-  // funcion para obtener el bio del actor por el id
-  function getUsuario( ActorId: string ) {
-      console.log({ ActorId });
+  function getMovieCastById( id: string ) {
+      console.log({ id });
+  }
+
+  function getActorBioById( id: string ) {
+      console.log({ id });
+  }
+
+  interface Movie {
+    cast:        string[],
+    description: string, 
+    rating:      number, 
+    title:       string,
   }
   
-  // Crear una película
-  function movie(title: string, description: string, rating: number, cast: string[] ) {
+  function createMovie({ title = '', description = '', rating = 0, cast = [] } : Movie) {
       console.log({ title, description, rating, cast });
   }
 
-  // Crea un nuevo actor
-  function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
+  function checkFullName(fullName: string): boolean {
+    console.log({ fullName });
+    return true;
+  }
+
+  function createActor( fullName: string, birthdate: Date ): boolean {
       
-      // tarea asincrona para verificar nombre
-      // ..
-      // ..
-      if ( fullName === 'fernando' ) return false;
+      if ( checkFullName(fullName) ) return false;
 
       console.log('Crear actor');
-      return true;        
-
+      return true;
   }
 
 })();
