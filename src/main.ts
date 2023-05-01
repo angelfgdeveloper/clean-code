95c31306a8f26a413dc20f52b0069f16deeb1c23
Luis Angel <angelfgdeveloper@gmail.com>
2023-05-01 11:06:05 -0600

    solid (s) - Single Responsibility Principle (SRP)

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import './style.css';
 // import './clean-code/06-clases-c';
 // import './clean-code/07-tarea';
 // import './code-smells/02-high-coupling';
-import './code-smells/02-low-coupling';
+// import './code-smells/02-low-coupling';
+import './solid/01-srp';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
