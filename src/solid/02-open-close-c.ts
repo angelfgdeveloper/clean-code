8719f10e9ba825a080394b7628002ce23192468b
Luis Angel <angelfgdeveloper@gmail.com>
2023-05-01 12:26:11 -0600

    solid (o) - Open/Closed Principle (OCP) - aplicado - detectar violaciones

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,11 +1,27 @@
-import axios from 'axios';
+// import axios from 'axios';
+// Eliminamos axios
 
+// Uso de principio open / close
 export class HttpClient {
 
+  // async get( url: string ) {
+  //   const { data, status } = await axios.get(url);
+  //   // console.log({ status });
+  //   return { data, status };
+  // }
+
   async get( url: string ) {
-    const { data, status } = await axios.get(url);
-    // console.log({ status });
-    return { data, status };
+    const resp = await fetch(url);
+    const data = await resp.json();
+
+    return { data, status: resp.status };
   }
 
 }
+
+/**
+ * Dectección de violaciones (OPC)
+ * 
+ * Cambios normales afectan nuestra clase o módulo.
+ * Cuando una clase o módulo afecta muchas capas. (Presentación, almacenamiento, etc.) 
+ */
