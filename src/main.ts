9eca749663df2e0f68009da1034121f5b1f8e1ba
Luis Angel <angelfgdeveloper@gmail.com>
2023-05-02 18:24:18 -0600

    solid (l) - Liskov Substitution Principle (LSP)

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ import './style.css';
 // import './code-smells/02-high-coupling';
 // import './code-smells/02-low-coupling';
 // import './solid/01-srp';
-import './solid/02-open-close-a';
+// import './solid/02-open-close-a';
+import './solid/03-liskov-a';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
